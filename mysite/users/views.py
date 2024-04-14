dcd770688898a7172943acb7b812b8bcfa1e9c88
Krisiris250592 <108283761+Krisiris250592@users.noreply.github.com>
2024-04-14 17:46:29 +0200

    New

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm, LoginForm
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def signupuser(request):
@@ -41,4 +44,10 @@ def logoutuser(request):
     logout(request)
     return redirect(to='noteapp:main')
 
-# Create your views here.
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
